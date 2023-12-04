b22ea01d93a2a66aa098b5e5e538f32070c9a326
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 15:44:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ config/Database.php
@@ -0,0 +1,8 @@
+<?php 
+    class Database { 
+        private $host = "localhost";
+        private $user = "user";
+        private $pass = "";
+        private $db_name = "mvc_login_tb";
+    }
+?>
