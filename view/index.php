506aff7265f0ec4c32aaea12147bbfc1e49fcd71
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 16:50:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- view/index.php
+++ view/index.php
@@ -6,7 +6,7 @@
     <h1 class="text-center">Welcome, To System</h1>
     <hr>
     <div class="text-center">
-        <a href=""><button class="btn btn-primary">Login</button></a>
+        <a href="login.php"><button class="btn btn-primary">Login</button></a>
         <a href="signup.php"><button class="btn btn-success">Register</button></a>
     </div>
 
