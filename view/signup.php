d90e43adc98853fe9fda67cadb9011a24e405e42
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 16:35:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ view/signup.php
@@ -0,0 +1,14 @@
+<?php 
+    include("../headers/header.php")
+    
+?>
+
+<div class="container">
+    <form action="" method="POST">
+        <label for="username">Username : </label>
+        <input type="text" name="userName" id="" class="form-control"><br>
+
+        <label for="email">Email : </label>
+        <input type="email" name="userEmail" id="" class="form-control">
+    </form>
+</div>
