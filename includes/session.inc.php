cca55164b5ffb5c61c2f7b4cb11f87d3ad1a1006
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 17:31:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-    function ViewError($name = '', $msg = '', $class = 'alert alert-warning alert-dismissible fade show'){
+    function ViewError($name = '', $msg = '', $class = 'alert alert-danger alert-dismissible fade show'){
         if(!empty($name)){
             if(!empty($msg) && empty($_SESSION[$name])){
                 $_SESSION[$name] = $msg;
@@ -11,6 +11,9 @@
             elseif(empty($msg) && !empty($_SESSION[$name])){
                 $class = !empty($_SESSION[$name.'_class']) ? $_SESSION[$name.'_class'] : $class;
                 echo "<div class='".$class."' role='alert'>
+                        <strong>ERROR : </strong>". $_SESSION[$nmae] . "
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                      </div>";
             } 
         }
     }
