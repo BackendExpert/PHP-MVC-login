07025e7e36deaa8eb795dfc656b1be3f8a1d6d5a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 19:26:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- view/index.php
+++ view/index.php
@@ -3,7 +3,14 @@
 ?>
 
 <div class="container">
-    <h1 class="text-center">Welcome, To System</h1>
+    <h1 class="text-center">Welcome, <?php
+        if(isset($_SESSION['usersId'])){
+            echo explode(" ", $_SESSION['userName'])[0];
+        }else{
+            echo "To System";
+        }
+    
+    ?></h1>
     <hr>
     <div class="text-center">
         <a href="login.php"><button class="btn btn-primary">Login</button></a>
