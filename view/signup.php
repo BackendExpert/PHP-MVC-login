6f20da034497615cf8acb8939a894a12afa3ea7f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 16:40:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- view/signup.php
+++ view/signup.php
@@ -9,6 +9,16 @@
         <input type="text" name="userName" id="" class="form-control"><br>
 
         <label for="email">Email : </label>
-        <input type="email" name="userEmail" id="" class="form-control">
+        <input type="email" name="userEmail" id="" class="form-control"><br>
+
+        <label for="password">Password : </label>
+        <input type="password" name="userPwd" id="" class="form-control"><br>
+
+        <label for="repeat_pass">Repeat Password : </label>
+        <input type="password" name="pwdRepeat" id="" class="form-control"><br>
+
+        <input type="reset" value="Clear" class="btn btn-secondary">
+        <input type="submit" value="SignUp" name="signup" class="btn btn-success">
+
     </form>
 </div>
