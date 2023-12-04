9a58db791ecc670b6c4af8f2af69a927b5b217fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 16:47:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ view/login.php
@@ -0,0 +1,21 @@
+<?php 
+    include("../headers/header.php");
+?>
+
+<style>
+    .card{
+        margin: 25px 0;
+    }
+    .bck-btn{
+        margin: 30px 0;
+    }
+</style>
+
+<div class="container">
+    <a href="index.php"><button class="btn btn-primary bck-btn">Back</button></a>
+    <div class="card">
+        <div class="card-header">
+            Login User
+        </div>
+    </div>
+</div>
