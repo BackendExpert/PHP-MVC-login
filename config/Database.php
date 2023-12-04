133e590a6b3a09f1fb07b02182a2b5a7465e019f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-04 15:47:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- config/Database.php
+++ config/Database.php
@@ -4,5 +4,15 @@
         private $user = "user";
         private $pass = "";
         private $db_name = "mvc_login_tb";
+
+        //PDO objects
+
+        private $dbh;
+        private $stmt;
+        private $error;
+
+        
     }
+
+
 ?>
